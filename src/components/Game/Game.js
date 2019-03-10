d13807981554987aa97e1f4e0fae64375947a8ca
Kanu Malivad <kanu.malivad@gmail.com>
2019-03-10 12:08:10 +0530

    React initial commit

---
+++ src/components/Game/Game.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+        return (
+            <h1>Welcome to React</h1>
+        );
+    }
+}
+export default Game;
